beabd74b17c8401fe8417047f2cb338087072cfe
Danilo V Carandang Jr <jrcaranddang@gmail.com>
2018-01-10 21:46:50 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 console.log('hello johnny james');
-=======
 console.log('hello johnny');
 
 console.log('hello test change 2');
 
 console.log('hello test change 3');
->>>>>>> 273e61a63e3eaf52d782de3dc3d0e99b04f13be5
